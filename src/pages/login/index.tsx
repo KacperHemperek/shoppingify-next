1a77ddce70deebf98d3fd8717b6d67b65e74da4b
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-04-15 10:45:37 +0200

    fix: catch json.parse error

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -100,10 +100,14 @@ function formatErrorMessage(
   /* eslint-disable  @typescript-eslint/no-explicit-any */
   error: TRPCClientErrorLike<any> | null
 ): string | undefined {
-  if (error && Array.isArray(JSON.parse(error.message))) {
-    return JSON.parse(error.message)[0].message;
+  try {
+    if (error && Array.isArray(JSON.parse(error.message))) {
+      return JSON.parse(error.message)[0].message;
+    }
+    return error?.message;
+  } catch (e) {
+    return error?.message;
   }
-  return error?.message;
 }
 
 function RegisterFormContent() {
